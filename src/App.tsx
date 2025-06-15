69e449b79d780f6ea9fe1db72ae6a7ebaf34a322
Aki Tada <3615622+torinoko@users.noreply.github.com>
2025-06-15 17:27:39 +0900

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="bg-main text-center">
+      <div className="container mx-auto py-10">
+        <img src="images/shiba.png" alt="柴犬と文鳥" className="w-full max-w-[60%] mx-auto h-auto" />
+        <p className="text-white mx-auto text-lg">Ruby とか TypeScript とか Linux とかで遊んでいる柴犬と文鳥です。</p>
+      </div>
+    </header>
+  )
+}
