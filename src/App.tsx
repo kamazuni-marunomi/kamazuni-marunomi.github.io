41f5c50a99f09765d2fc47c4111ac2de52d34d46
Aki Tada <3615622+torinoko@users.noreply.github.com>
2025-06-15 21:34:59 +0900

    Add Profile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Header from './components/Header';
+import Profile from './components/Profile';
 
 export default function App() {
   return (
     <>
       <Nav />
       <Header />
+      <Profile />
     </>
   );
 }
