0fb95b09ff26dd901b68d5c6d42e584e255efcec
tadaaki <torinoko@users.noreply.github.com>
2025-06-16 18:30:37 +0900

    Changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kamazuni-marunomi.github.io/',
+  base: '/',
   build: {
     outDir: 'docs',
   },
