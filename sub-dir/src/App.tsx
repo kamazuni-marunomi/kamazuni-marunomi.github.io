f64ae26f92329de1dbcc98998ea464077bc01ab6
tadaaki <torinoko@users.noreply.github.com>
2025-06-16 11:29:42 +0900

    create sub-dir

---
+++ sub-dir/src/App.tsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav';
+import Header from './components/layout/Header';
+import Profile from './components/Profile/Profile';
+import Portfolio from './components/Portfolio/Portfolio';
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Profile />
+      <Portfolio />
+    </>
+  );
+}
