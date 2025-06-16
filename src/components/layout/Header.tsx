442e75dff691ad694b6c9eb49e6a578b76a8c39a
tadaaki <torinoko@users.noreply.github.com>
2025-06-16 19:15:44 +0900

    Change paths.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-main text-center">
       <div className="container mx-auto py-10">
-        <img src="images/shiba.png" alt="柴犬と文鳥" className="w-full max-w-[60%] mx-auto h-auto" />
+        <img src="/images/shiba.png" alt="柴犬と文鳥" className="w-full max-w-[60%] mx-auto h-auto" />
         <p className="text-white text-lg mt-8">Ruby とか TypeScript とか Linux とかで遊んでいる柴犬と文鳥です。</p>
       </div>
     </header>
