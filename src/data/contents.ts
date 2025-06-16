e10e0b6cc2efe393a2c2c9dd80f64b1ed50f4fa4
tadaaki <torinoko@users.noreply.github.com>
2025-06-16 19:52:35 +0900

    Change deploy.yml.

--- src/data/contents.ts
+++ src/data/contents.ts
@@ -1,42 +1,42 @@
 const contentsOop = {
   title: 'プログラミング勉強中の人にオブジェクト指向とは何なのかなんとなく伝えたい本',
   summary: 'オブジェクト指向プログラミングについてイラスト多めでふわっと解説した本です。',
-  image: '/images/oop_cover.jpg',
+  image: 'images/oop_cover.jpg',
   url: 'https://kamazunimarunomi.booth.pm/items/666660'
 };
 
 const contentsLinux = {
   title: 'ふんわりさっくりLinuxカーネル',
   summary: 'Linux カーネルの初歩の初歩についてイラスト多めでふわっと解説した本です。',
-  image: '/images/linux_cover.jpg',
+  image: 'images/linux_cover.jpg',
   url: 'https://kamazunimarunomi.booth.pm/items/1044107'
 };
 
 const contentsWork = {
   title: '文鳥と読む労働法',
   summary: '労働法について紹介している本です。文鳥はあまり出てきません。',
-  image: '/images/roudou_cover.jpg',
+  image: 'images/roudou_cover.jpg',
   url: 'https://kamazunimarunomi.booth.pm/items/1044119'
 };
 
 const contentsWeb = {
   title: '小さな会社でWebプログラマはじめました',
   summary: 'Web プログラマーになった女の子の4コマまんがです。',
-  image: '/images/web_cover.jpg',
+  image: 'images/web_cover.jpg',
   url: 'https://kamazunimarunomi.booth.pm/items/666590'
 };
 
 const contentsWaaah = {
   title: 'Waaah',
   summary: '予定日から妊娠月数の日付を取得する gem です。',
-  image: '/images/waaah.png',
+  image: 'images/waaah.png',
   url: 'https://rubygems.org/gems/waaah'
 };
 
 const contentsBuncho = {
   title: 'Bucho',
   summary: '文鳥の体重を記録する gem です。',
-  image: '/images/buncho.png',
+  image: 'images/buncho.png',
   url: 'https://rubygems.org/gems/buncho'
 };
 
